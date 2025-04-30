c4cae7279df9e6f3fb54f2a4f498c0f63487359c
devsujoydas <devsujoydas@gmail.com>
2025-04-30 13:26:46 +0600

    upload routers and update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// imports packages
 const express = require('express');
 const path = require('path');
-
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+// imports database connection and Routers from local
+const db = require('./config/mongoose-connection');
+const onwersRouter = require('./routes/onwersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
+
+
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
@@ -16,7 +23,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")))
 
-
+app.use("/onwers", onwersRouter)
+app.use("/users", usersRouter)
+app.use("/products", productsRouter)
 
 app.get("/", async (req, res) => {
     res.render("index")
---
+++ routes/onwersRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res)=>{
+    res.send("hey this is onwerRouter")
+})
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
